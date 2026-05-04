29055718b0e68feacabe0355812ccc51e1a7a58e
Leonardo Gambaroni <leogambaroni@gmail.com>
2026-05-04 11:11:11 -0300

    Delete Tecnologia e Programação para dispositivos móveis/Estudos P2 Mateus directory

--- Tecnologia e Programação para dispositivos móveis/Estudos P2 Mateus/exercicio6.dart
+++
@@ -1,9 +0,0 @@
-// fazer uma funcao que calcula o dobro
-
-int dobro (int numero){
-  return numero * 2;
-}
-
-void main(){
-  print(dobro(5));
-}
--- Tecnologia e Programação para dispositivos móveis/Estudos P2 Mateus/exercicio9.dart
+++
@@ -1,9 +0,0 @@
-//inverter lista
-
-void main(){
-
-List<int> lista = [1, 2, 3, 4, 5];
-
-print(lista.reversed);
-
-}
